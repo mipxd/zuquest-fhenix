a138c129b61deefc07f9ce885fbd6438c70cc39d
Thomas <thomasr928@gmail.com>
2024-12-09 22:37:18 +0700

    Add basic stake contract

--- src/FHERC20.sol
+++ src/FHERC20.sol
@@ -2,32 +2,44 @@
 pragma solidity ^0.8.19;
 
 import { FHERC20 } from "@fhenixprotocol/contracts/experimental/token/FHERC20/FHERC20.sol";
-import { inEuint128 } from "@fhenixprotocol/contracts/FHE.sol";
+import { inEuint128, euint128, FHE } from "@fhenixprotocol/contracts/FHE.sol";
+import { inEuint32, euint32 } from "@fhenixprotocol/contracts/FHE.sol";
 import { AccessControl } from "@openzeppelin/contracts/access/AccessControl.sol";
 
 error FHERC20NotAuthorized();
 
-contract ExampleToken is FHERC20, AccessControl {
-    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
-    uint256 public lol;
+contract ZuquestStakes is FHERC20 {
+    mapping (string => euint128) public eventVotes;
+    mapping (address => mapping(string => euint128)) public userVotes;
+    uint256 public deadline;
 
-    constructor(string memory name, string memory symbol, uint256 initialBalance) FHERC20(name, symbol) {
-        lol = initialBalance;
-        // _mintEncrypted(msg.sender, encryptedBalance);
-        _grantRole(MINTER_ROLE, msg.sender);
-        _mint(msg.sender, initialBalance);
+    constructor(string memory name, string memory symbol) FHERC20(name, symbol) {
+        deadline = block.timestamp + 7 days;
     }
 
     function mintEncrypted(address recipient, inEuint128 memory amount) public {
-        if (hasRole(MINTER_ROLE, msg.sender)) {
-            _mintEncrypted(recipient, amount);
-        } else {
-            revert FHERC20NotAuthorized();
-        }
+        _mintEncrypted(recipient, amount);
     }
 
-    function mint(address _address, uint256 _amount) public {
-        _mint(_address, _amount);
+    function getMyBidZeroPrivacy(address account, string memory eventName) external view returns (uint256) {
+        return FHE.decrypt(userVotes[account][eventName]);
     }
+
+    function stake(address _address, string memory eventName, inEuint128 memory amount) external {
+        userVotes[_address][eventName] = FHE.asEuint128(amount);
+        eventVotes[eventName] = FHE.add(eventVotes[eventName], FHE.asEuint128(amount));
+    }
+
+    function getInterest(string memory eventName) external view returns (uint256) {
+        // If deadline has passed, anyone can decrypt interest
+        require(block.timestamp > deadline, "Deadline has not passed");
+        return FHE.decrypt(eventVotes[eventName]);
+    }
+
+    // Want to confirm we're able to call functions before trying to handle encrypted data
+    function mintEncryptedFake(address recipient, uint256 amount) external {}
+
+    function stakeFake(address _address, string memory eventName, uint256 amount) external {}
+
 }
--- test/FHERC20.t.sol
+++ test/FHERC20.t.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.8.25 <0.9.0;
 
 import { Test } from "forge-std/src/Test.sol";
 
-import { ExampleToken, FHERC20NotAuthorized } from "../src/FHERC20.sol";
+import { ZuquestStakes, FHERC20NotAuthorized } from "../src/FHERC20.sol";
 import { FheEnabled } from "../util/FheHelper.sol";
 import { Permission, PermissionHelper } from "../util/PermissionHelper.sol";
 
@@ -16,17 +16,17 @@ interface IERC20 {
 /// @dev If this is your first time with Forge, read this tutorial in the Foundry Book:
 /// https://book.getfoundry.sh/forge/writing-tests
 contract TokenTest is Test, FheEnabled {
-    ExampleToken internal token;
+    ZuquestStakes internal token;
     PermissionHelper private permitHelper;
 
     address public owner;
     uint256 public ownerPrivateKey;
 
-    uint256 private receiverPrivateKey;
-    address private receiver;
+    uint256 private testerPrivateKey;
+    address private tester;
 
     Permission private permission;
-    Permission private permissionReceiver;
+    Permission private permissionTester;
 
     /// @dev A function invoked before each test case is run.
     function setUp() public virtual {
@@ -35,8 +35,8 @@ contract TokenTest is Test, FheEnabled {
         initializeFhe();
         // *****************************************************
 
-        receiverPrivateKey = 0xB0B;
-        receiver = vm.addr(receiverPrivateKey);
+        testerPrivateKey = 0xB0B;
+        tester = vm.addr(testerPrivateKey);
 
         ownerPrivateKey = 0xA11CE;
         owner = vm.addr(ownerPrivateKey);
@@ -44,72 +44,36 @@ contract TokenTest is Test, FheEnabled {
         vm.startPrank(owner);
 
         // Instantiate the contract-under-test.
-        token = new ExampleToken("hello", "TST", 10_000_000);
+        token = new ZuquestStakes("ZuQuestToken", "ZQT");
         permitHelper = new PermissionHelper(address(token));
 
         permission = permitHelper.generatePermission(ownerPrivateKey);
-        permissionReceiver = permitHelper.generatePermission(receiverPrivateKey);
+        permissionTester = permitHelper.generatePermission(testerPrivateKey);
 
         vm.stopPrank();
     }
 
-    /// @dev Basic test for the balanceOf function of the plaintext ERC20.
-    function testBalanceOf() external {
-        assertEq(0, token.balanceOf(msg.sender));
-        uint256 toMint = 1.0 * 10 ^ token.decimals();
-        token.mint(msg.sender, toMint);
-        assertEq(token.balanceOf(msg.sender), toMint);
-    }
-
-    // @dev Failing test for mintEncrypted function with unauthorized minter
-    function testMintEncryptedNoPermissions() public {
-        uint128 value = 50;
-        inEuint128 memory inputValue = encrypt128(value);
-
-        vm.expectRevert(FHERC20NotAuthorized.selector);
-        token.mintEncrypted(owner, inputValue);
-    }
 
     // @dev Test mintEncrypted function with authorized minter
     function testMintEncrypted() public {
         uint128 value = 50;
+        vm.startPrank(tester);
         inEuint128 memory encryptedValue = encrypt128(value);
-
-        vm.prank(owner);
-        token.mintEncrypted(owner, encryptedValue);
-
-        string memory encryptedBalance = token.balanceOfEncrypted(owner, permission);
+        token.mintEncrypted(tester, encryptedValue);
+        string memory encryptedBalance = token.balanceOfEncrypted(tester, permissionTester);
         uint256 balance = unseal(address(token), encryptedBalance);
         assertEq(balance, uint256(value));
+        vm.stopPrank();
     }
 
-    // @dev Test transferEncrypted function - tests reading and writing encrypted balances and using permissions
-    function testTransferEncrypted() public {
+    function testStake() public {
+        vm.startPrank(tester);
         uint128 value = 50;
         inEuint128 memory encryptedValue = encrypt128(value);
-
-        vm.startBroadcast(owner);
-
-        token.mintEncrypted(owner, encryptedValue);
-
-        string memory encryptedBalance = token.balanceOfEncrypted(owner, permission);
-        uint256 balance = unseal(address(token), encryptedBalance);
+        token.stake(tester, "test", encryptedValue);
+        uint256 balance = token.getMyBidZeroPrivacy(tester, "test");
         assertEq(balance, uint256(value));
-
-        uint128 transferValue = 10;
-
-        inEuint128 memory encryptedTransferValue = encrypt128(transferValue);
-        euint128 transferred = token.transferEncrypted(receiver, encryptedTransferValue);
-        assertEq(transferred.decrypt(), transferValue);
-
-        string memory encryptedBalanceAfterTransfer = token.balanceOfEncrypted(owner, permission);
-        uint256 balanceAfterTransfer = unseal(address(token), encryptedBalanceAfterTransfer);
-        assertEq(balanceAfterTransfer, uint256(value - transferValue));
-
-        string memory encryptedBalanceReceiver = token.balanceOfEncrypted(receiver, permissionReceiver);
-        uint256 balanceReceiver = unseal(address(token), encryptedBalanceReceiver);
-        assertEq(balanceReceiver, uint256(transferValue));
-
-        vm.stopBroadcast();
+        vm.stopPrank();
     }
+
 }
